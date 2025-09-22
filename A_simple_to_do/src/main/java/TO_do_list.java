2b70b7073cf6d4f886a8bfadbbf23cd7899236ad
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2025-09-22 10:38:50 -0500

    initial

---
+++ A_simple_to_do/src/main/java/TO_do_list.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TO_do_list {
+
+    ArrayList<String> action_list = new ArrayList<String>();                    // main data structure
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ A_simple_to_do/src/main/java/main.java
@@ -0,0 +1,18 @@
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
